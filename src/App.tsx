de5b5e54645d0c8b2fd3dea8c9dc4299b946b6ef
Ishukk44 <45058860+Ishukk@users.noreply.github.com>
2025-03-12 23:49:29 +0530

    first commit, tailwind added

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center h-screen bg-gray-200">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello, Tailwind with Vite!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
