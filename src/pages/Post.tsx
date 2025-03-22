25e8a13bb5deeb1538e639c8af174441a22bec18
Ishukk44 <45058860+Ishukk@users.noreply.github.com>
2025-03-22 23:01:46 +0530

    post design crated

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -3,16 +3,28 @@ import { useEffect, useState } from "react";
 import Navbar from "../components/navbar/Navbar";
 import Sidebar from "../components/sidebar/Sidebar";
 import axios from "axios";
+import { FiSend } from "react-icons/fi";
+import { FaHeart, FaRegComment } from "react-icons/fa";
+import { JSX } from "react/jsx-runtime";
+
+interface PostType {
+  map(arg0: (post: any) => JSX.Element): import("react").ReactNode;
+  id: number;
+  title: string;
+  body: string;
+  reactions: any;
+  views: number;
+}
 
 function Post() {
-  const [posts, setPosts] = useState<
-    { id: number; title: string; body: string }[]
-  >([]);
+  const [posts, setPosts] = useState<PostType>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const postResponse = await axios.get("https://dummyjson.com/posts");
+        const postResponse = await axios.get(
+          "https://dummyjson.com/posts?limit=2"
+        );
         setPosts(postResponse.data.posts);
       } catch (error) {
         console.log(error);
@@ -22,26 +34,50 @@ function Post() {
     fetchData();
   }, []);
 
-  console.log("---------------posttttttt", posts);
+ 
   return (
     <div className="flex">
       <div className="w-full h-screen">
         <Navbar />
         <div className="flex">
-          <div className="w-2/12 bg-blue-500">
+          <div className="w-2/12 ">
             <Sidebar />
           </div>
-          <div className="w-10/12 bg-blue-400">
-            <div className=" w-1/1 items-center justify-center h-screen bg-gray-200">
-              <div className="bg-gray-50 h-screen w-1/1 border-2 border-red-200">
-                {posts?.map((e) => (
-                  <div key={e.id}>
-                    <h1>{e.title}</h1>
-                    <p>{e.body}</p>
+          <div className="w-10/12 bg-gray-200">
+            {posts?.map((post) => (
+              <div className="flex justify-center mt-2  ">
+                <div className=" w-1/1 items-center justify-center   ">
+                  <div className="bg-gray-50 w-1/1  p-2 border-2 rounded-lg ">
+                    <div className=" flex  ">
+                      <div className="max-w-sm rounded-lg shadow-sm ">
+                        <div className="h-[400px]">
+                          <img
+                            className="rounded-t-lg h-[400px]"
+                            src="https://random-image-pepebigotes.vercel.app/api/random-image"
+                            alt=""
+                            loading="lazy"
+                          />
+                        </div>
+                        <div className="p-2">
+                          <div className=" flex justify-between">
+                            <div className="flex">
+                              <FaHeart className="w-6 h-6 text-current p-1" />
+                              <FaRegComment className="w-6 h-6 text-current p-1" />
+                              <FiSend className="w-6 h-6 text-current p-1" />
+                            </div>
+                            <p className="text-sm">Views {post.views}</p>
+                          </div>
+                          <a href="#" className="text-xs">
+                            Likes {post.reactions.likes}
+                          </a>
+                          <p className="text-sm">{post.title}</p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                ))}
+                </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
