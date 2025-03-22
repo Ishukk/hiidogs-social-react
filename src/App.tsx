81640d142659585eae344ba2934d2ae17c39ee67
Ishukk44 <45058860+Ishukk@users.noreply.github.com>
2025-03-22 12:20:06 +0530

    sidebar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import Navbar from "./components/navbar/Navbar";
-
+import Sidebar from "./components/sidebar/Sidebar";
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="flex items-center justify-center h-screen bg-gray-200">
-        <h1 className="text-4xl font-bold text-blue-600">
-          Hello, Tailwind with Vite!
-        </h1>
+    <div className="flex">
+      <div className="w-full h-screen">
+        <Navbar />
+        <div className="flex">
+          <div className="w-2/12 bg-blue-500">
+            <Sidebar />
+          </div>
+          <div className="w-10/12 bg-blue-400">
+            <div className=" w-1/1 items-center justify-center h-screen bg-gray-200">
+              <h1 className="text-4xl font-bold text-blue-600">
+                Hello, Tailwind with Vite!
+              </h1>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
